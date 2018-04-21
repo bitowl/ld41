063822336c6d2e203b3324a45da42b89535a9fe3
bitowl <github@bitowl.de>
2018-04-21 10:54:06 +0200

    Add super simple player and enemy models

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float HorizontalSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position + Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * HorizontalSpeed;
+	}
+}
