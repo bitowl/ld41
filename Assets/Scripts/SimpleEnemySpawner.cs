480e06f2811f2fd96d54f523f5d63ad1603cbe85
bitowl <github@bitowl.de>
2018-04-21 12:43:37 +0200

    Begin RTS prototype

---
+++ Assets/Scripts/SimpleEnemySpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemySpawner : MonoBehaviour {
+
+	public Transform[] spawnPoints;
+	public float minSpawnInterval = 0.1f;
+	public float maxSpawnInterval = 1;
+	public GameObject enemyPrefab;
+
+	private float spawnTimer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spawnTimer -= Time.deltaTime;
+
+		if (spawnTimer <= 0) {
+			SpawnEnemy(spawnPoints[Random.Range(0, spawnPoints.Length)]);
+			spawnTimer = Random.Range(minSpawnInterval, maxSpawnInterval);
+		}
+	}
+
+	void SpawnEnemy(Transform spawnPoint) {
+		Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+	}
+}
